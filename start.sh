e36f601fab75f22ef50271cd8a42515d2b03198d
Mhhydra25 <96157167+Mhhydra25@users.noreply.github.com>
2023-11-20 16:40:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/Mhhydra25/AutoFilterAdvance009 /AutoFilterAdvance009
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  git clone $UPSTREAM_REPO /AutoFilterAdvance009
 fi
-cd /AutoFilterAdvance
+cd /AutoFilterAdvance009
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
